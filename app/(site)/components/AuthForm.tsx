f711d8b03700a75a02a32c9f22cd4ac6bb689e47
serg988 <42653178+serg988@users.noreply.github.com>
2023-05-10 18:43:12 +0300

    Login form UI created

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,7 +1,12 @@
 'use client'
 
+import Input from '@/app/components/inputs/Input'
 import { useCallback, useState } from 'react'
-import { FieldValue, FieldValues, SubmitHandler, useForm } from 'react-hook-form'
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
+import Button from '@/app/components/Button'
+import AuthSocialButton from './AuthSocialButton'
+
+import { BsGithub, BsGoogle } from 'react-icons/bs'
 
 type Variant = 'LOGIN' | 'REGISTER'
 
@@ -17,38 +22,105 @@ const AuthForm = () => {
     }
   }, [variant])
 
-	const {
-		register,
-		handleSubmit,
-		formState: {
-			errors
-		}
-	} = useForm<FieldValues>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
     defaultValues: {
       name: '',
       email: '',
       password: '',
     },
-	})
-	
-	const onSubmit: SubmitHandler<FieldValues> = data => {
-		setIsLoading(true)
+  })
 
-		if (variant === 'REGISTER') {
-			//TODO Axios register
-		}
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true)
 
-		if (variant === 'LOGIN') {
-			//TODO NextAuth Sign In
-		}
-	}
+    if (variant === 'REGISTER') {
+      //TODO Axios register
+    }
 
-	const socialAction = (action: string) => {
-		setIsLoading(true)
+    if (variant === 'LOGIN') {
+      //TODO NextAuth Sign In
+    }
+  }
 
-		//TODO NextAuth social Sign In
-	}
+  const socialAction = (action: string) => {
+    setIsLoading(true)
 
-  return <div>AuthForm!!</div>
+    //TODO NextAuth social Sign In
+  }
+
+  return (
+    <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
+      <div className='bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10'>
+        <form className='space-y-6' onSubmit={handleSubmit(onSubmit)}>
+          {variant === 'REGISTER' && (
+            <Input
+              id='name'
+              label='Name'
+              register={register}
+              errors={errors}
+              disabled={isLoading}
+            />
+          )}
+          <Input
+            id='email'
+            label='Email address'
+            register={register}
+            type='email'
+            errors={errors}
+            disabled={isLoading}
+          />
+          <Input
+            id='password'
+            label='Password'
+            register={register}
+            type='password'
+            errors={errors}
+            disabled={isLoading}
+          />
+          <div>
+            <Button disabled={isLoading} fullWidth type='submit'>
+              {variant === 'REGISTER' ? 'Register' : 'Sign in'}
+            </Button>
+          </div>
+        </form>
+        <div className='mt-6'>
+          <div className='relative'>
+            <div className='absolute inset-0 flex items-center'>
+              <div className='w-full border-t border-gray-300' />
+            </div>
+            <div className='relative flex justify-center text-sm'>
+              <span className='bg-white px-2 text-gray-500'>
+                Or continue with
+              </span>
+            </div>
+          </div>
+          <div className='mt-6 flex gap-2'>
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction('github')}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction('google')}
+            />
+          </div>
+        </div>
+        <div className='flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500'>
+          <div>
+            {variant === 'LOGIN'
+              ? 'New to messenger?'
+              : 'Already have an account?'}
+          </div>
+          <div onClick={toggleVariant} className='underline cursor-pointer'>
+            {variant === 'LOGIN' ? 'Create an account' : 'Login'}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 export default AuthForm
